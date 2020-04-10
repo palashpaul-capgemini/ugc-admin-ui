2fffbcc6bfe81ad652cbd759fa1ae12ba34be32c
Rolandas Gedgaudas-Trockis | Capgemini <rolandas.gedgaudas-trockis@capgemini.com>
2020-04-10 19:14:49 +0200

    Country model added.

---
+++ models/Country.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const Country = db.define(
+	'countrylookup',
+	{
+		countrycode: {
+			type: Sequelize.STRING,
+			primaryKey: true,
+			required: true,
+			unique: true,
+		},
+		countryname: {
+			type: Sequelize.STRING,
+			required: true,
+		},
+		enable: {
+			type: Sequelize.INTEGER,
+		},
+	},
+	{
+		schema: 'master',
+		freezeTableName: true,
+		timestamps: false,
+	}
+);
+
+module.exports = User;
